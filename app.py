450077b89922eddae696ade2a589d3133267c059
jcembik97 <133568028+jcembik97@users.noreply.github.com>
2023-06-20 15:40:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
-from markupsafe import escape
+"""
+docstring
+"""
+
+from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """
+    docstring
+    """
+
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+if __name__ == "__main__":
+    app.run(debug=True)
